e59ac9d4080eedbe7826a3e0236b538a53aa7682
Angel Hernandez fonseca <angelhernandezfonseca@MacBook-Pro-de-Angel.local>
2025-01-14 11:55:36 -0600

    Impresion de variables

---
+++ impresion_variables.py
@@ -0,0 +1,15 @@
+""" impresion de variables """
+nombre = "juan"
+print(nombre)
+
+numero_uno = 10
+numero_dos = 5
+
+""" impresion de sumas """
+print(numero_uno+numero_dos)
+
+""" impresion de concatenaciones """
+nombre_completo = "juan angel"
+apellidos_completos = "hernandez fonseca"
+
+print(nombre_completo + " " + apellidos_completos)
